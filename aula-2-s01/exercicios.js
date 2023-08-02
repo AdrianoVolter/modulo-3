b69dee6849d53174d1159448b54da03fe2aa21a8
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-08-01 21:30:26 -0300

    aula 2 modulo 3 ,iniciando SOLID

---
+++ aula-2-s01/exercicios.js
@@ -0,0 +1,30 @@
+class Pedido {
+    constructor(id, cliente){
+        this.nome = id;
+        this.preco = cliente;
+        this.itens = [];
+        this.valorTotal = 0;
+    }
+
+    calcularValorTotal(){
+        this.valorTotal = this.itens.reduce((total, item) => total + item.precoUnitario, 0);
+    }
+
+    adicionarItem(item, precoUnitario, quantidade){
+        this.itens.push({
+            item,
+            precoUnitario,
+            quantidade
+        });
+    }
+}
+
+const pedido = new Pedido(1, 'João');
+pedido.adicionarItem('Coca-Cola', 5, 2);
+pedido.adicionarItem('Batata Frita', 10, 1);
+pedido.calcularValorTotal();
+console.log(pedido);
+
+
+
+
