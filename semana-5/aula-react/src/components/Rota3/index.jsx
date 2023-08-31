88ec82462bc698039fc941cadce812fb1fa7a13d
adrianovolter <adriano.j.volter@hotmail.com>
2023-08-31 12:39:46 -0300

    revisando react,

--- semana-5/aula-react/src/components/Rota3/index.jsx
+++ semana-5/aula-react/src/components/Rota3/index.jsx
@@ -1,20 +1,38 @@
+import './Rota3.styles.css';
+ 
+function Rota3() {
 
-function Rota3 () {
   return (
-    <div
-    style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-      width: "100vw",
-      backgroundColor: "lightblue",
-  }}
-    >
-      <h1>Rota 3</h1>
-      <p>Esta é a rota 3</p>
+    <div className="rota3">
+      <div className='topo'>
+        <h2>Unidades Geradoras</h2>
+      </div>
+      <form action="">
+        <label htmlFor="nome">Unidade Geradora</label>
+        <select 
+          name="unidade" 
+          id="unidade"
+          value="unidade"
+          >
+          <option value="1">Unidade 1</option>
+          {/* {unidades.map((unidade) => (
+            <option value={unidade.id}>{unidade.nome}</option>
+          ))} */}
+        </select>
+        <label htmlFor="">Mes/ano</label>
+        <input type="month" name="" id="data"/>
+        <label htmlFor="">Total kw gerado</label>
+        <button
+          type="submit"
+          className="btn-cadastrar"
+          
+        >
+          Cadastrar
+        </button>
+      </form>
+
     </div>
-  )
+  );
 }
 
 export default Rota3;
