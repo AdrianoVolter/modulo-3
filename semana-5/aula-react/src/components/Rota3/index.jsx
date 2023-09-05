f86f5a556d751706856efbab1e52daa93a72944c
adrianovolter <adriano.j.volter@hotmail.com>
2023-09-05 20:21:48 -0300

    Semana 6 , teste TDD

--- semana-5/aula-react/src/components/Rota3/index.jsx
+++ semana-5/aula-react/src/components/Rota3/index.jsx
@@ -3,30 +3,7 @@ import "./Rota3.styles.css";
 function Rota3() {
   return (
     <div className="rota3">
-      <div className="topo">
-        <h2>Lançamento de geração mensal</h2>
-      </div>
-      <div className="formulario">
-        <form>
-          <div className='unidade-geradora'>
-            <label>Unidade Geradora</label>
-            <select name="unidade" id="unidade" value="unidade">
-              <option value="1">Unidade 1</option>
-            </select>
-          </div>
-          <div>
-            <label>Mes/ano</label>
-            <input type="month" name="" id="data" />
-          </div>
-          <div>
-            <label>Total kw gerado</label>
-            <input type="number" name="total" id="kw" />
-          </div>
-          <button type="submit" className="btn-cadastrar">
-            Cadastrar
-          </button>
-        </form>
-      </div>
+      <h1>Rota 3</h1>
     </div>
   );
 }
