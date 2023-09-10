a691ef1d6575706b1399dbe06fa1e8755013003d
adrianovolter <adriano.j.volter@hotmail.com>
2023-09-10 18:54:10 -0300

    teste em react , alterando par ingles

--- semana-6/aula-react/src/App.spec.jsx
+++ semana-6/aula-react/src/App.spec.jsx
@@ -2,61 +2,54 @@ import { getByText, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-describe("deve redenrizar os componentes", () => {
-  test('deve renderizar o o texto "Hello"', () => {
+describe("tests App.jsx", () => {
+  test('should be render the text "Hello"', () => {
     render(<App />);
     const linkElement = screen.getByText(/Hello/i);
     expect(linkElement).toBeInTheDocument();
   });
 
-  test('deve renderizar o o texto "digite seu nome"', () => {
+  test('should be render the text "enter your name"', () => {
     render(<App />);
     const inputElement = screen.getByPlaceholderText(/digite seu nome/i);
     expect(inputElement).toBeInTheDocument();
   });
 
-  test("deve renderizar a imagem", () => {
+  test("should be render the image", () => {
     render(<App />);
     const imgElement = screen.queryByRole("img");
     expect(imgElement).toBeInTheDocument();
   });
 });
 
-describe("deve encontrar a imagem com texto alternativo correto", () => {
-  test("deve encontrar a imagem com texto alternativo correto", () => {
-    render(<App />);
-    const imgElement = screen.getByAltText(
-      "ilustração de uma mulher negra usando o computador e segurando uma xícara"
-    );
-    expect(imgElement).toBeInTheDocument();
-  });
+test("Should be find the image with correct alt text", () => {
+  render(<App />);
+  const imgElement = screen.getByAltText(
+    "ilustração de uma mulher negra usando o computador e segurando uma xícara"
+  );
+  expect(imgElement).toBeInTheDocument();
+});
 
-  test("Deve pegar o input corretamente", async () => {
-    render(<App />);
-    const inputElement = screen.queryByPlaceholderText(/digite seu nome/i);
+test("should be get the input correctly", async () => {
+  render(<App />);
+  const inputElement = screen.queryByPlaceholderText(/digite seu nome/i);
 
-    userEvent.type(inputElement, "João");
+  userEvent.type(inputElement, "João");
 
-    await waitFor(() => {
-      const nameElement = screen.getByText(/João/i);
-      expect(nameElement).toBeInTheDocument();
-    });
-  });
-
-  test("Deve mostrar o nome digitado abaixo do input", async () => {
-    render(<App />);
-    const inputElement = screen.queryByPlaceholderText(/digite seu nome/i);
-
-    userEvent.type(inputElement, "João");
-    
-  
-    await waitFor(() => {
-      
-      expect(screen.getByText(/João/i)).toBeInTheDocument();
-      expect(screen.getByText(/hello/i)).toBeInTheDocument("Hello, João");
-    }
-    );
+  await waitFor(() => {
+    const nameElement = screen.getByText(/João/i);
+    expect(nameElement).toBeInTheDocument();
   });
 });
 
-    
+test("should be show the name entered below the input", async () => {
+  render(<App />);
+  const inputElement = screen.queryByPlaceholderText(/digite seu nome/i);
+
+  userEvent.type(inputElement, "João");
+
+  await waitFor(() => {
+    expect(screen.getByText(/João/i)).toBeInTheDocument();
+    expect(screen.getByText(/hello/i)).toBeInTheDocument("Hello, João");
+  });
+});
